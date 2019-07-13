6e243f2a8a756c980eaed94751fca53bfdb82f0c
JoshuaDESKTOP <jc.grant22@gmail.com>
2019-07-13 13:22:23 -0700

    Fixed build script

--- api/app.js
+++ api/app.js
@@ -20,7 +20,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../build')));
+
+
+  app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  });
+}
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
